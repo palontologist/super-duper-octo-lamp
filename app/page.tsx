f891e106dfdbd15381911d3f3a86d2777165a93e
installer <installer@gmail.com>
2025-08-09 11:02:46 +0300

    Integrate MorphingText component into Page for dynamic text presentation, update header and paragraph content for improved messaging and alignment with core mission.

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@
 import { Navigation } from "@/components/ui/navigation";
 import { WaitlistForm } from "@/components/ui/waitlist-form";
 //import Image from "next/image";
+import { MorphingText } from "@/components/magicui/morphing-text";
 import { ComicText } from "@/components/magicui/comic-text";
 
 export default function Page() {
@@ -27,10 +28,10 @@ export default function Page() {
         {/* Left column */}
         <div className="flex-1 flex flex-col items-center justify-center p-4 sm:p-8">
           <h1 className="text-3xl sm:text-4xl md:text-6xl lg:text-7xl font-extrabold text-center text-white mb-4 sm:mb-6">
-            It&apos;s time to <span className="block">measure your impact.</span>
+            Align your daily actions with <span className="block">your core mission</span>
           </h1>
           <p className="text-base sm:text-lg md:text-2xl text-center text-white/80 max-w-2xl px-4">
-            The unified platform to measure your total impact.<br />From carbon footprint to community action.
+          The OS for <MorphingText texts={["Founders", "Experts" , "Students", "Teams"]} /> Focus.
           </p>
         </div>
         {/* Divider */}
@@ -42,7 +43,7 @@ export default function Page() {
                   <ComicText fontSize={5}>TRY IT!</ComicText>
           </div>
             <p className="text-sm sm:text-base md:text-lg text-white/80 mt-2 sm:mt-4 max-w-xl text-center mb-6 sm:mb-8 px-4">
-              It&apos;s free to <a href="https://greta-v1.vercel.app" target="_blank" rel="noopener noreferrer" className="underline text-blue-300 hover:text-blue-400 hover:scale-105 transition-transform duration-200">get started</a> for early access.
+              It&apos;s free to get started for early access.
             </p>
             
             {/* Waitlist Form */}
